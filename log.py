85ae65f4d55b260ca92ad54dedb6af53c81060a9
lzg129 <507097321@qq.com>
2026-04-08 14:48:37 +0800

    day1

--- log.py
+++ log.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-content = input("今天做了啥：")
+content = input("待到秋来九月八：")
 
 with open("log.txt", "a", encoding="utf-8") as f:
     f.write(f"{datetime.now()} - {content}\n")
